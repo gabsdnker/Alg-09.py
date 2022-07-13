34b451bd205ec134d12a6b502c1f84a38cce0ef4
gabi <107625531+gabsdnker@users.noreply.github.com>
2022-07-13 17:19:17 -0300

    Update GD-Alg-09-Ex-04.py

--- GD-Alg-09-Ex-04.py
+++ GD-Alg-09-Ex-04.py
@@ -4,13 +4,3 @@
 #Escreva um programa Python que apresente o mesmo comportamento.
 #O programa deve exibir uma mensagem de erro caso algum arquivo não puder ser lido, e então passa para a leitura do próximo arquivo. 
 #O programa também deve exibir mensagem de erro caso não tenha sido passado nenhum argumento para o programa em linha de comando.
-
-import sys 
-
-if __name__=='__main__':
-
-    try:
-        
-    arquivo.close()
-    except FileNotFoundError:
-       print("ERRO ESSE ARQUIVO NÃO EXITE")
