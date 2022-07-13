42bc10dc6e3f4cc03e4b3198a65dded105955ef8
gabi <107625531+gabsdnker@users.noreply.github.com>
2022-07-13 09:43:37 -0300

    Add files via upload

---
+++ GD-Alg-09-Ex-03.py
@@ -0,0 +1,28 @@
+#Exibir a "cauda" de um arquivo texto.
+
+#Escreva um programa Python que apresente o mesmo comportamento. O programa deve exibir uma mensagem de erro caso não exista o arquivo ou caso não tenha sido passado nenhum argumento para o programa em linha de comando.
+
+#terminal: tail
+#Ultima 10 palavras 
+# úmidos
+# única
+# únicas
+# único
+# únicos
+# úrico
+# úteis
+# útero
+# úteros
+# útil
+
+import sys 
+
+if __name__=='__main__':
+
+    try:
+        arquivo= open(sys.argv, "r")
+        x= arquivo.readline()
+        
+        arquivo.close()
+    except FileNotFoundError:
+        print("ERRO ESSE ARQUIVO NÃO EXITE")
