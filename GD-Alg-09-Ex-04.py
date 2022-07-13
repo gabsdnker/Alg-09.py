f7447eb62f5238148964186d008ba84c348b5471
gabi <107625531+gabsdnker@users.noreply.github.com>
2022-07-13 17:19:28 -0300

    Delete GD-Alg-09-Ex-04.py

--- GD-Alg-09-Ex-04.py
+++
@@ -1,6 +0,0 @@
-#Concatenar múltiplos arquivos.
-
-#terminal: cat
-#Escreva um programa Python que apresente o mesmo comportamento.
-#O programa deve exibir uma mensagem de erro caso algum arquivo não puder ser lido, e então passa para a leitura do próximo arquivo. 
-#O programa também deve exibir mensagem de erro caso não tenha sido passado nenhum argumento para o programa em linha de comando.
