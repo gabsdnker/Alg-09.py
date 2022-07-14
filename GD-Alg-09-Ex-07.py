9852d8cd20accef893ee4113282ed33dfd626603
gabi <107625531+gabsdnker@users.noreply.github.com>
2022-07-14 12:53:42 -0300

    Update GD-Alg-09-Ex-07.py

--- GD-Alg-09-Ex-07.py
+++ GD-Alg-09-Ex-07.py
@@ -5,11 +5,34 @@
 #Seu programa não deve diferenciar letras maiúsculas e minúsculas (portento deve tratar A e a como sendo a mesma letra).
 #O usuário deve fornecer o nome do arquivo como argumento em linha de comando.
 
-from curses.ascii import isalpha
-import numbers
+def frequencia_de_letras(arquivo):
+    with open(arquivo, "r") as fonte:
+        while x:=fonte.readline():
+            lista = []
+            for letra in x:
+                a = letra.lower()
+                if a.isalpha() == True:
+                    lista.append(a)
+                alfabeto = set(lista)
 
-
-with open("bla.txt", 'r') as fonte:
-    while letras:= fonte.readline(1):
-        if letras == " " and letras == numbers and letras == isalpha():
-            
+            dicionario = {}
+            n = 0
+            for letra in alfabeto:
+                for item in lista:
+                    if letra == item:
+                        n += 1
+                dicionario[letra] = ((n/len(lista)))
+                n = 0
+            lista1 = []
+            for valor in sorted(dicionario, key = dicionario.get, reverse=True):
+                lista1.append("{0:2}:{1:6.0%}".format(valor, dicionario[valor]))
+            return lista
+    
+def main():
+    arquivo = input("Arquivo: ")
+    lista = (frequencia_de_letras(arquivo))            
+    for item in lista:
+        print(item)
+    
+if __name__ == "__main__":
+    main()
